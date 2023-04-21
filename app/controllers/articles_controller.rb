902a2e37ef358306d77747aa9217b7bdfaa048e0
Colleen <colleenrust524@EMAIL.com>
2023-04-21 14:25:08 -0500

    allow search for keywords in articles on httpie

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,13 @@
 class ArticlesController < ApplicationController
   def index
-    web_response = HTTP.get("https://newsapi.org/v2/everything?domains=wsj.com&q#{params[:search]}&apiKey=#{Rails.application.credentials.news_api.api_key}").parse(:json)
+    web_response = HTTP.get("https://newsapi.org/v2/everything?domains=wsj.com&q=#{params[:search]}&apiKey=#{Rails.application.credentials.news_api.api_key}").parse(:json)
     articles = web_response["articles"]
-    # filtered_articles = []
-    # articles.each do |article|
-    #   if article["title"][0] == "R"
-    #     filtered_articles << articles    
-    #   end
-    # end
+    filtered_articles = []
+    articles.each do |article|
+      if article["title"][0] == "R"
+        filtered_articles << articles
+      end
+    end
     render json: web_response
   end
 end
